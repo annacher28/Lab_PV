6212b42aad614105261f6d64974d0e6e08392146
Anna <ch3repanovaann@yandex.ru>
2025-12-06 11:26:27 +0300

    Лаба 4 исправление

--- task_4.js
+++ task_4.js
@@ -5,19 +5,19 @@ class ExpressionCalculator {
 
     y2(a, b) {
         if (a === b) {
-            throw new Error("Ошибка: деление на ноль (a - b = 0)");
+            throw new Error("Делить на 0 нельзя (a - b = 0)");
         }
         return (a + b) / (a - b);
     }
 
     y3(a, x, b) {
         if (b === 0) {
-            throw new Error("Ошибка: деление на ноль (b = 0)");
+            throw new Error("Делить на 0 нельзя (b = 0)");
         }
         const value = (a * x) / b;
         
         if (!Number.isInteger(value) || value < 0) {
-            throw new Error("Ошибка: факториал определён только для неотрицательных целых чисел");
+            throw new Error("Факториал только для неотрицательных целых чисел - ошибка");
         }
         
         return this.F(value);
@@ -37,8 +37,8 @@ const calc1 = new ExpressionCalculator();
 const calc2 = new ExpressionCalculator();
 const calc3 = new ExpressionCalculator();
 
-console.log("calc1: y = 3x + 5, x = 4 →", calc1.y1(4)); // 17
+console.log("y = 3x + 5, x = 4 Получится: ", calc1.y1(4)); 
 
-console.log("calc2: y = (a+b)/(a-b), a=10, b=5 →", calc2.y2(10, 5)); // 3
+console.log("y = (a+b)/(a-b), a=10, b=5 Получится: ", calc2.y2(10, 5)); 
 
-console.log("calc3: y = (ax/b)!, a=2, x=6, b=3 →", calc3.y3(2, 6, 3)); // 4! = 24
+console.log("y = (ax/b)!, a=2, x=6, b=3 Получится: ", calc3.y3(2, 6, 3)); 
